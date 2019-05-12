e508f111a0838c1c046a385a780f2320fa5ac782
GonzoM321 <gonzalo2370@yahoo.com>
2019-05-12 10:47:54 -0500

    update

--- Front-End/src/app/hair-search/hair-search.component.ts
+++ Front-End/src/app/hair-search/hair-search.component.ts
@@ -8,6 +8,8 @@ import {
 
 import { Hair } from '../hair';
 import { HairService } from '../hair.service';
+import { HairCutService } from '../haircut.services';
+import { updoService } from '../updo.service';
 
 @Component({
   selector: 'app-hair-search',
@@ -17,6 +19,10 @@ import { HairService } from '../hair.service';
 export class HairSearchComponent implements OnInit {
   hairs$: Observable<Hair[]>;
   private searchTerms = new Subject<string>();
+  haircutServices: any;
+  haircut$: Observable<{}>;
+  updo$: Observable<Hair[]>;
+  updoService: any;
 
   constructor(private hairService: HairService) {}
 
@@ -35,6 +41,26 @@ export class HairSearchComponent implements OnInit {
 
       
       switchMap((term: string) => this.hairService.searchHairs(term)), 
+    );
+    this.haircut$ = this.searchTerms.pipe(
+      
+      debounceTime(300),
+
+      
+      distinctUntilChanged(),
+
+      
+      switchMap((term: string) => this.haircutServices.searchHairCuts(term)), 
+    );
+    this.updo$ = this.searchTerms.pipe(
+      
+      debounceTime(300),
+
+      
+      distinctUntilChanged(),
+
+      
+      switchMap((term: string) => this.updoService.searchupdos(term)), 
     );
       
       }
